6f1dcfc9331c18f0b089dbb5e28680b99fddb258
Matt Bone <thatmattbone@gmail.com>
2012-11-29 21:18:28 -0600

    rename stub file

--- WhereIsComputation/whereiscomputation/views.py
+++ WhereIsComputation/whereiscomputation/views.py
@@ -33,33 +33,33 @@ After you fix the problem, please restart the Pyramid application to
 try it again.
 """
 
-@view_config(route_name='precomputed', renderer="templates/stub.jinja2")
+@view_config(route_name='precomputed', renderer="templates/primes.jinja2")
 def precomputed(request):
     return {'name': 'Precomputed Primes',
             'primes': ", ".join(ONE_HUNDRED_PRIME_STRINGS)}
 
 
-@view_config(route_name='serverside', renderer="templates/stub.jinja2")
+@view_config(route_name='serverside', renderer="templates/primes.jinja2")
 def serverside(request):
     return {'name': 'Primes computed on the server-side',
             'primes': ", ".join([str(prime) for prime in sieve(ONE_HUNDRED_PRIMES[-1])])}
 
 
-@view_config(route_name='serverside-with-cache', renderer="templates/stub.jinja2")
+@view_config(route_name='serverside-with-cache', renderer="templates/primes.jinja2")
 def serverside_with_cache(request):
     return {'name': 'Primes computed on the server-side with a caching layer'}
 
 
-@view_config(route_name='clientside', renderer="templates/stub.jinja2")
+@view_config(route_name='clientside', renderer="templates/primes.jinja2")
 def clientside(request):
     return {'name': 'Primes computed on the client-side'}
 
 
-@view_config(route_name='server-and-clientside', renderer="templates/stub.jinja2")
+@view_config(route_name='server-and-clientside', renderer="templates/primes.jinja2")
 def server_and_clientside(request):
     return {'name': 'Primes computed on the client and server-side'}
 
 
-@view_config(route_name='database-layer', renderer="templates/stub.jinja2")
+@view_config(route_name='database-layer', renderer="templates/primes.jinja2")
 def database_layer(request):
     return {'name': 'Primes computed in the database layer'}
