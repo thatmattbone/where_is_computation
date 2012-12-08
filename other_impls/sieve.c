1dec7db7b86aba58315d39516346ffe83d2ffd27
Matt Bone <thatmattbone@gmail.com>
2012-12-08 07:33:35 -0600

    c impl

---
+++ other_impls/sieve.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#define MAX_PRIME 100000
+
+void sieve(int max_n, short primes[MAX_PRIME]) {
+  short zero = 0;
+  memset(primes, zero, MAX_PRIME);
+
+  for(int n=2; n<max_n; n++) {
+    int current_n = n + n;
+    while(current_n < max_n) {
+      primes[current_n] = 1;
+      current_n += n;
+    }
+  }
+}
+
+int main(void) {
+  int max_n = 100;
+  short primes[MAX_PRIME];
+  assert(max_n < MAX_PRIME);
+  
+  sieve(100, primes);
+  
+  for(int i=0; i<max_n; i++) {
+    printf("%i, ", primes[i]);  
+  }
+  printf("\n");
+  
+  return 0;
+}
---
+++ redis/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://redis.googlecode.com/files/redis-2.6.7.tar.gz
+tar xzf redis-2.6.7.tar.gz
+cd redis-2.6.7
+make
