9df64282cceff1da661ad117c9efd8b7d86c60a1
Matt Bone <thatmattbone@gmail.com>
2012-12-07 22:09:54 -0600

    stub for creating functions in postgres

--- WhereIsComputation/whereiscomputation/scripts/initializedb.py
+++ WhereIsComputation/whereiscomputation/scripts/initializedb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import transaction
+from zope.sqlalchemy import mark_changed
 
 from sqlalchemy import engine_from_config
 
@@ -21,6 +22,19 @@ def usage(argv):
           '(example: "%s development.ini")' % (cmd, cmd)) 
     sys.exit(1)
 
+my_one_function = """
+CREATE OR REPLACE FUNCTION my_one()
+  RETURNS integer AS
+$BODY$
+one = 1
+return one
+$BODY$
+  LANGUAGE plpythonu VOLATILE
+  COST 100;
+-- ALTER FUNCTION my_one()
+--   OWNER TO whereiscomputation;
+"""
+
 def main(argv=sys.argv):
     if len(argv) != 2:
         usage(argv)
@@ -30,6 +44,15 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+
+
+    session = DBSession()
+    conn = session.connection()
+    conn.execute(my_one_function)
+    mark_changed(session)
+    transaction.commit()
+
+    #with transaction.manager:
+    #    model = MyModel(name='one', value=1)
+    #    DBSession.add(model)
+    #    import pdb; pdb.set_trace()
